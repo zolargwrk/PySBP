4830e015a02007a3cb55caf54fb5a168c225a983
Zelalem <zelalem.worku@mail.utoronto.ca>
2019-09-02 20:31:54 -0400

    CSBP added

--- solver/advection_solver.py
+++ solver/advection_solver.py
@@ -33,5 +33,5 @@ def advection_solver_1d(p, xl, xr, nelem, t0, tf, a, quad_type, flux_type = 'Cen
 
 
 # advection_solver_1d(p, xl, xr, nelem, t0, tf, a, quad_type, flux_type = 'Central')
-u = advection_solver_1d(3, 0, 2, 2, 0, 5, 2*np.pi, 'CSBP', 'Upwind', n=40)
+u = advection_solver_1d(4, 0, 2, 2, 0, 5, 2*np.pi, 'LG', 'Upwind', n=40)
 

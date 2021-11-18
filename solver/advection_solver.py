70ba9986fcad005e53bfc54ab035b0512109e72c
Zelalem <zolargwrk@gmail.com>
2021-11-17 19:56:10 -0500

    advection_steady runs but does not give correct results

--- solver/advection_solver.py
+++ solver/advection_solver.py
@@ -127,7 +127,7 @@ def advection_solver_2d(p, h, t0, tf, cfl=1, flux_type='Upwind', boundary_type=N
 # u = advection_solver_2d(2, 0.5, 0, 1, cfl=1, flux_type='Upwind', boundary_type='nPeriodic')
 
 
-def advection_solver_sbp_2d_steady(p, h, sbp_family='diagE'):
+def advection_solver_sbp_2d_steady(p, h, nrefine=1, sbp_family='diagE', p_map=1, curve_mesh=False):
     dim = 2
     nface = dim + 1
     nfp = p + 1
@@ -138,209 +138,161 @@ def advection_solver_sbp_2d_steady(p, h, sbp_family='diagE'):
     bB = 0
     bT = 1
 
+    upwind = 0 # upwind = 1 gives upwind flux
     # generate mesh
     mesh = MeshGenerator2D.rectangle_mesh(h, bL, bR, bB, bT)
-    btype = ['d', 'd', 'd', 'd']
+    domain_type = 'notperiodic'
+    if domain_type == 'notperiodic':
+        btype = ['d', 'd', 'd', 'd']
+    elif domain_type == 'periodic':
+        btype = ['-', '-', '-', '-']
 
-    ass_data = Assembler.assembler_sbp_2d(p, mesh, btype, sbp_family)
+    ass_data = Assembler.assembler_sbp_2d(p, mesh, btype, sbp_family, p_map=p_map, curve_mesh=curve_mesh,
+                                          domain_type=domain_type)
     adata = SimpleNamespace(**ass_data)
-    x = adata.x
-    y = adata.y
-    nelem = adata.nelem
-    nnodes = adata.nnodes
-    bgrp = adata.bgrp
-    bgrpD = adata.bgrpD
-    rx = adata.rx
-    ry = adata.ry
-    sx = adata.sx
-    sy = adata.sy
-    Dr = adata.Dr
-    Ds = adata.Ds
-    surf_jac = adata.surf_jac
-    nx = adata.nx
-    ny = adata.ny
-    jac = adata.jac
-    R1 = adata.R1
-    R2 = adata.R2
-    R3 = adata.R3
-    B1 = adata.B1
-    B2 = adata.B2
-    B3 = adata.B3
-    etoe = adata.etoe
-    etof = adata.etof
+    errs_soln = list()
+    errs_adj = list()
+    errs_func = list()
+    hs = list()
+    nelems = list()
+    nnodes_list = list()
+    cond_nums = list()
+    nnz_elems = list()
+    eig_vals = list()
 
-    H = adata.H
+    # refine mesh
+    for refine in range(0, nrefine):
+        if refine == 0:
+            mesh = MeshGenerator2D.rectangle_mesh(h, bL, bR, bB, bT)
+        else:
+            # mesh = MeshGenerator2D.rectangle_mesh(h, bL, bR, bB, bT, True)
+            mesh = MeshTools2D.hrefine_uniform_2d(ass_data, bL, bR, bB, bT)
 
-    u = 0 * x
+        # update assembled data for 2D implementation
+        ass_data = Assembler.assembler_sbp_2d(p, mesh, btype, sbp_family, p_map, curve_mesh=curve_mesh,
+                                              domain_type=domain_type)
+        adata = SimpleNamespace(**ass_data)
 
-    # face id
-    fid1 = np.arange(0, nfp)
-    fid2 = np.arange(nfp, 2 * nfp)
-    fid3 = np.arange(2 * nfp, 3 * nfp)
+        # extract variables from adata
+        x = adata.x
+        y = adata.y
+        r = adata.r
+        s = adata.s
+        nelem = adata.nelem
+        nnodes = adata.nnodes
+        vx = adata.vx
+        vy = adata.vy
+        xf = adata.xf
+        yf = adata.yf
+        Lx = adata.Lx  # length of domain in the x direction (not Lambda)
+        Ly = adata.Ly  # length of domain in the y direction (not Lambda)
+        etov = adata.etov
+        if domain_type == 'notperiodic':
+            etoe = adata.etoe
+            etof = adata.etof
+        elif domain_type == 'periodic':
+            etoe = adata.etoe_periodic
+            etof = adata.etof_periodic
+        # initialize solution vectors
+        u = 0 * x
 
-    # boundary group (obtain element number and facet number only)
-    bgrp = np.vstack(bgrp)[:, 2:4]
-    # Dirichlet boundary groups by facet
-    bgrpD1 = bgrpD2 = bgrpD3 = []
-    if len(bgrpD) != 0:
-        bgrpD1 = bgrpD[bgrpD[:, 1] == 0, :]
-        bgrpD2 = bgrpD[bgrpD[:, 1] == 1, :]
-        bgrpD3 = bgrpD[bgrpD[:, 1] == 2, :]
+        # get operators on physical elements
+        phy_data = MeshTools2D.map_operators_to_phy_2d(p, nelem, adata.H, adata.Dr, adata.Ds, adata.Er, adata.Es,
+                                                       adata.R1, adata.R2, adata.R3, adata.B1, adata.B2, adata.B3,
+                                                       adata.rx, adata.ry, adata.sx, adata.sy, adata.jac,
+                                                       adata.surf_jac, adata.nx, adata.ny)
+        phy = SimpleNamespace(**phy_data)
 
-    # get the geometric factors for each element (in rxB, B stands for Block)
-    rxB = rx.T.reshape(nelem, nnodes, 1)
-    ryB = ry.T.reshape(nelem, nnodes, 1)
-    sxB = sx.T.reshape(nelem, nnodes, 1)
-    syB = sy.T.reshape(nelem, nnodes, 1)
 
-    # get volume and surface Jacobians for each elements
-    jacB = jac.T.reshape(nelem, nnodes, 1)
-    surf_jac1B = surf_jac[fid1, :].flatten(order='F').reshape(nelem, nfp, 1)
-    surf_jac2B = surf_jac[fid2, :].flatten(order='F').reshape(nelem, nfp, 1)
-    surf_jac3B = surf_jac[fid3, :].flatten(order='F').reshape(nelem, nfp, 1)
+        # advection coefficients
+        IB = np.block([np.eye(nfp)] * nelem).T.reshape(nelem, nfp, nfp).transpose(0, 2, 1)
+        LrB = IB
+        LsB = IB
 
-    # get the normal vectors on each facet
-    # nx1B = nx[fid1, :].flatten(order='F').reshape(nelem, nfp, 1)
-    # ny1B = ny[fid1, :].flatten(order='F').reshape(nelem, nfp, 1)
-    #
-    # nx2B = nx[fid2, :].flatten(order='F').reshape(nelem, nfp, 1)
-    # ny2B = ny[fid2, :].flatten(order='F').reshape(nelem, nfp, 1)
-    #
-    # nx3B = nx[fid3, :].flatten(order='F').reshape(nelem, nfp, 1)
-    # ny3B = ny[fid3, :].flatten(order='F').reshape(nelem, nfp, 1)
+        # get the advection coefficient
+        Ln1B  = phy.nxB[0] * LrB + phy.nyB[0] * LsB
+        Ln2B  = phy.nxB[1] * LrB + phy.nyB[1] * LsB
+        Ln3B  = phy.nxB[2] * LrB + phy.nyB[2] * LsB
+        Ln = [Ln1B, Ln2B, Ln3B]
 
-    nx1B = np.repeat(nx[fid1, :].flatten(order='F').reshape(nelem, nfp, 1)[:,0,:], nelem, axis=1).reshape(nelem, nelem, 1)
-    ny1B = np.repeat(ny[fid1, :].flatten(order='F').reshape(nelem, nfp, 1)[:,0,:], nelem, axis=1).reshape(nelem, nelem, 1)
+        # upwind SATs
+        T1kkB = 1/2*(phy.RB[0].transpose(0, 2, 1) @ phy.BB[0] @ (Ln[0] - upwind*np.abs(Ln[0])) @ phy.RB[0])
+        T2kkB = 1/2*(phy.RB[1].transpose(0, 2, 1) @ phy.BB[1] @ (Ln[1] - upwind*np.abs(Ln[1])) @ phy.RB[1])
+        T3kkB = 1/2*(phy.RB[2].transpose(0, 2, 1) @ phy.BB[2] @ (Ln[2] - upwind*np.abs(Ln[2])) @ phy.RB[2])
+        Tkk = [T1kkB, T2kkB, T3kkB]
 
-    nx2B = np.repeat(nx[fid2, :].flatten(order='F').reshape(nelem, nfp, 1)[:,0,:], nelem, axis=1).reshape(nelem, nelem, 1)
-    ny2B = np.repeat(ny[fid2, :].flatten(order='F').reshape(nelem, nfp, 1)[:,0,:], nelem, axis=1).reshape(nelem, nelem, 1)
+        HB_inv = np.linalg.inv(phy.HB)
+        # construct the system matrix
+        A = (np.block(np.zeros((nelem * nnodes, nelem * nnodes)))).reshape((nelem, nelem, nnodes, nnodes))
 
-    nx3B = np.repeat(nx[fid3, :].flatten(order='F').reshape(nelem, nfp, 1)[:,0,:], nelem, axis=1).reshape(nelem, nelem, 1)
-    ny3B = np.repeat(ny[fid3, :].flatten(order='F').reshape(nelem, nfp, 1)[:,0,:], nelem, axis=1).reshape(nelem, nelem, 1)
+        A_diag = (phy.DxB + phy.DyB) - HB_inv @ (Tkk[0] + Tkk[1] + Tkk[2])
 
-    # get the derivative operator on the physical elements and store it for each element
-    DrB = np.block([Dr] * nelem).T.reshape(nelem, nnodes, nnodes).transpose(0, 2, 1)
-    DsB = np.block([Ds] * nelem).T.reshape(nelem, nnodes, nnodes).transpose(0, 2, 1)
-    DxB = rxB * DrB + sxB * DsB
-    DyB = ryB * DrB + syB * DsB
+        # add the diagonals of the SAT matrix
+        for i in range(0, nelem):
+            A[i, i, :, :] += A_diag[i, :, :]
 
-    R1B = np.block([R1] * nelem).T.reshape(nelem, nnodes, nfp).transpose(0, 2, 1)
-    R2B = np.block([R2] * nelem).T.reshape(nelem, nnodes, nfp).transpose(0, 2, 1)
-    R3B = np.block([R3] * nelem).T.reshape(nelem, nnodes, nfp).transpose(0, 2, 1)
+        # subtract interface SATs added at boundary facets
+        for i in range(0, len(adata.bgrpD)):
+            elem = adata.bgrpD[i, 0]
+            face = adata.bgrpD[i, 1]
+            A[elem, elem, :, :] += HB_inv[elem, :, :] @ Tkk[face][elem, :, :]
 
-    RB = [R1B, R2B, R3B]
+        # add the interface SATs at the neighboring elements
+        for i in range(0, nelem):
+            if i != etoe[i, 0]:
+                # facet 1
+                face = 0
+                elem = i
+                nbr_elem = etoe[i, 0]
+                nbr_face = etof[i, 0]
+                A[i, nbr_elem, :, :] += HB_inv[elem, :, :] @ (1/2 * phy.RB[face].transpose(0, 2, 1)[elem, :, :] @ phy.BB[face][elem, :, :]
+                                                           @ np.flipud(np.fliplr(Ln[nbr_face][nbr_elem, :, :] - upwind*np.abs(Ln[face][elem, :, :])))
+                                                           @ np.flipud(np.fliplr(phy.RB[nbr_face][nbr_elem, :, :])))
+            if i != etoe[i, 1]:
+                # facet 2
+                face = 1
+                elem = i
+                nbr_elem = etoe[i, 1]
+                nbr_face = etof[i, 1]
+                A[i, nbr_elem, :, :] += HB_inv[elem, :, :] @ (1/2 * phy.RB[face].transpose(0, 2, 1)[elem, :, :] @ phy.BB[face][elem, :, :]
+                                                           @ np.flipud(np.fliplr(Ln[nbr_face][nbr_elem, :, :] - upwind*np.abs(Ln[face][elem, :, :])))
+                                                           @ np.flipud(np.fliplr(phy.RB[nbr_face][nbr_elem, :, :])))
+            if i != etoe[i, 2]:
+                # facet 3
+                face = 2
+                elem = i
+                nbr_elem = etoe[i, 2]
+                nbr_face = etof[i, 2]
+                A[i, nbr_elem, :, :] += HB_inv[elem, :, :] @ (1/2 * phy.RB[face].transpose(0, 2, 1)[elem, :, :] @ phy.BB[face][elem, :, :]
+                                                           @ np.flipud(np.fliplr(Ln[nbr_face][nbr_elem, :, :] - upwind*np.abs(Ln[face][elem, :, :])))
+                                                           @ np.flipud(np.fliplr(phy.RB[nbr_face][nbr_elem, :, :])))
 
-    # get volume norm matrix and its inverse on physical elements
-    HB = jacB * np.block([H] * nelem).T.reshape(nelem, nnodes, nnodes).transpose(0, 2, 1)
-    HB_inv = np.linalg.inv(HB)
+        # add boundary SATs
+        for i in range(0, len(adata.bgrpD)):
+            elem = adata.bgrpD[i, 0]
+            face = adata.bgrpD[i, 1]
+            A[elem, elem, :, :] -= HB_inv[elem, :, :] @ (phy.RB[face].transpose(0, 2, 1)[elem, :, :] @ phy.BB[face][elem, :, :]
+                                                                @ (Ln[face][elem, :, :] - upwind*np.abs(Ln[face][elem, :, :]))
+                                                                @ phy.RB[face][elem, :, :])
 
-    IB = np.block([np.eye(H.shape[0])] * nelem).T.reshape(nelem, nnodes, nnodes).transpose(0, 2, 1)
+        sD = (np.block(np.zeros((nelem * nnodes, nelem * nnodes)))).reshape((nelem, nelem, nnodes, nnodes))
+        for i in range(0, len(adata.bgrpD)):
+            elem = adata.bgrpD[i, 0]
+            face = adata.bgrpD[i, 1]
+            sD[elem, elem, :, :] += HB_inv[elem, :, :] @ (phy.RB[face].transpose(0, 2, 1)[elem, :, :] @ phy.BB[face][elem, :, :]
+                                                              @ (Ln[face][elem, :, :] - upwind*np.abs(Ln[face][elem, :, :]))
+                                                             @ phy.RB[face][elem, :, :])
 
-    # get surface norm matrix for each facet of each element
-    BB1 = surf_jac1B * np.block([B1] * nelem).T.reshape(nelem, nfp, nfp).transpose(0, 2, 1)
-    BB2 = surf_jac2B * np.block([B2] * nelem).T.reshape(nelem, nfp, nfp).transpose(0, 2, 1)
-    BB3 = surf_jac3B * np.block([B3] * nelem).T.reshape(nelem, nfp, nfp).transpose(0, 2, 1)
-
-    BB = [BB1, BB2, BB3]
-
-    # advection coefficients
-    LxB = IB
-    LyB = IB
-
-    # get advection coefficient in the reference coordinate
-    LrB = jacB*rxB * LxB + jacB*ryB * LyB
-    LsB = jacB*sxB * LxB + jacB*syB * LyB
-
-    # get the advection coefficient
-    Lgk1B = nx1B * LrB + ny1B * LsB
-    Lgk2B = nx2B * LrB + ny2B * LsB
-    Lgk3B = nx3B * LrB + ny3B * LsB
-    Lgk = [Lgk1B, Lgk2B, Lgk3B]
-
-    # get advection coefficient normal to the shared face
-    Ln1B = RB[0] @ LrB @ RB[0].transpose(0, 2, 1) + RB[0] @ LsB @ RB[0].transpose(0, 2, 1)
-    Ln2B = -RB[1] @ LrB @ RB[1].transpose(0, 2, 1)
-    Ln3B = -RB[2] @ LsB @ RB[2].transpose(0, 2, 1)
-    Ln = [Ln1B, Ln2B, Ln3B]
-
-    # coefficients for symmetric SATs
-    # T1kkB = 1 / 2 *(R1B.transpose(0, 2, 1) @ BB1 @ R1B @ Lgk[0])
-    # T2kkB = 1 / 2 *(R2B.transpose(0, 2, 1) @ BB2 @ R2B @ Lgk[1])
-    # T3kkB = 1 / 2 *(R3B.transpose(0, 2, 1) @ BB3 @ R3B @ Lgk[2])
-
-    # upwind SATs
-    T1kkB = 1/2*(R1B.transpose(0, 2, 1) @ BB1 @ R1B @ Lgk1B - R1B.transpose(0, 2, 1) @ np.abs(BB1 @ Ln1B) @ R1B)
-    T2kkB = 1/2*(R2B.transpose(0, 2, 1) @ BB2 @ R2B @ Lgk2B - R2B.transpose(0, 2, 1) @ np.abs(BB2 @ Ln2B) @ R2B)
-    T3kkB = 1/2*(R3B.transpose(0, 2, 1) @ BB3 @ R3B @ Lgk3B - R3B.transpose(0, 2, 1) @ np.abs(BB3 @ Ln3B) @ R3B)
-    Tkk = [T1kkB, T2kkB, T3kkB]
-
-    # construct the system matrix
-    A = (np.block(np.zeros((nelem * nnodes, nelem * nnodes)))).reshape((nelem, nelem, nnodes, nnodes))
-
-    A_diag = 1/2*(DxB @ LxB + DyB @ LyB) + 1/2*(rxB * LxB @ DrB + sxB * LxB @ DsB)\
-             + 1/2*(ryB * LyB @ DrB + syB * LyB @ DsB) - HB_inv @ (Tkk[0] + Tkk[1] + Tkk[2])
-
-    # add the diagonals of the SAT matrix
-    for i in range(0, nelem):
-        A[i, i, :, :] += A_diag[i, :, :]
-
-    # subtract interface SATs added at boundary facets
-    for i in range(0, bgrp.shape[0]):
-        elem = bgrp[i, 0]
-        face = bgrp[i, 1]
-        A[elem, elem, :, :] += 1 * HB_inv[elem, :, :] @ Tkk[face][elem, :, :]
-
-    # add the interface SATs at the neighboring elements
-    for i in range(0, nelem):
-        if i != etoe[i, 0]:
-            # facet 1
-            nbr_elem = etoe[i, 0]
-            nbr_face = etof[i, 0]
-            # A[i, nbr_elem, :, :] -= HB_inv[i, :, :] @ (1/2 * R1B.transpose(0, 2, 1)[i, :, :] @ BB1[i, :, :]
-            #                                            @Ln[nbr_face][nbr_elem, :, :] @ RB[nbr_face][nbr_elem, :, :])
-            A[i, nbr_elem, :, :] -= HB_inv[i, :, :] @ (1/2 * R1B.transpose(0, 2, 1)[i, :, :]
-                                                       @ (BB1[i, :, :] @ Ln[nbr_face][nbr_elem, :, :]
-                                                          + np.abs(BB1[i, :, :] @ Ln[nbr_face][nbr_elem, :, :]))
-                                                       @ RB[nbr_face][nbr_elem, :, :])
-        if i != etoe[i, 1]:
-            # facet 2
-            nbr_elem = etoe[i, 1]
-            nbr_face = etof[i, 1]
-            # A[i, nbr_elem, :, :] -= HB_inv[i, :, :] @ (1/2 * R2B.transpose(0, 2, 1)[i, :, :] @ BB2[i, :, :]
-            #                                            @Ln[nbr_face][nbr_elem, :, :] @ RB[nbr_face][nbr_elem, :, :])
-            A[i, nbr_elem, :, :] -= HB_inv[i, :, :] @ (1/2 * R2B.transpose(0, 2, 1)[i, :, :]
-                                                       @ (BB2[i, :, :] @ Ln[nbr_face][nbr_elem, :, :]
-                                                          + np.abs(BB2[i, :, :] @ Ln[nbr_face][nbr_elem, :, :]))
-                                                       @ RB[nbr_face][nbr_elem, :, :])
-        if i != etoe[i, 2]:
-            # facet 3
-            nbr_elem = etoe[i, 2]
-            nbr_face = etof[i, 2]
-            # A[i, nbr_elem, :, :] -= HB_inv[i, :, :] @ (1/2 * R3B.transpose(0, 2, 1)[i, :, :] @ BB3[i, :, :]
-            #                                            @Ln[nbr_face][nbr_elem, :, :] @ RB[nbr_face][nbr_elem, :, :])
-            A[i, nbr_elem, :, :] -= HB_inv[i, :, :] @ (1/2 * R3B.transpose(0, 2, 1)[i, :, :]
-                                                       @ (BB3[i, :, :] @ Ln[nbr_face][nbr_elem, :, :]
-                                                          + np.abs(BB3[i, :, :] @ Ln[nbr_face][nbr_elem, :, :]))
-                                                       @ RB[nbr_face][nbr_elem, :, :])
-
-    # add boundary SATs
-    for i in range(0, len(bgrpD)):
-        elem = bgrpD[i, 0]
-        face = bgrpD[i, 1]
-        # A[elem, elem, :, :] -= HB_inv[elem, :, :] @ (1/2*RB[face].transpose(0, 2, 1)[elem, :, :] @ BB[face][elem, :, :]
-        #                                              @ Ln[face][elem, :, :] @ RB[face][elem, :, :])
-        A[elem, elem, :, :] -= HB_inv[elem, :, :] @ (RB[face].transpose(0, 2, 1)[elem, :, :] @ BB[face][elem, :, :]
-                                                         @ RB[face][elem, :, :] @ Lgk[face][elem, :, :]
-                                                         - RB[face].transpose(0, 2, 1)[elem, :, :]
-                                                         @ np.abs(BB[face][elem, :, :] @ Ln[face][elem, :, :])
-                                                         @ RB[face][elem, :, :])
-
-    f = (np.pi * np.cos(np.pi * x) * np.sin(np.pi*y) + np.pi * np.cos(np.pi * y) * np.sin(np.pi*x))
+    u_exact = np.sin(np.pi * x) * np.sin(np.pi * y)
     A_mat = (A.transpose(0, 2, 1, 3)).reshape(nelem * nnodes, nelem * nnodes)
     A_mat = sparse.csr_matrix(A_mat)
-    u = spsolve(A_mat, f.reshape((nnodes * nelem, 1), order='F'))
+    sD_mat = (sD.transpose(0, 2, 1, 3)).reshape(nelem * nnodes, nelem * nnodes)
+    sD_mat = sparse.csr_matrix(sD_mat)
+    sDf = sD_mat @ u_exact.reshape(-1, 1, order='F')
+    f = (np.pi * np.cos(np.pi * x) * np.sin(np.pi*y) + np.pi * np.cos(np.pi * y) * np.sin(np.pi*x)).reshape(-1, 1, order='F') + sDf
+    u = spsolve(A_mat, f)
     u = u.reshape(nelem, nnodes).T
-    u_exact = np.sin(np.pi * x) * np.sin(np.pi * y)
+
     err = np.linalg.norm(u - u_exact)
 
     plot_figure_2d(x, y, u_exact)
@@ -349,4 +301,4 @@ def advection_solver_sbp_2d_steady(p, h, sbp_family='diagE'):
     return u
 
 
-u = advection_solver_sbp_2d_steady(2, 0.5, 'diagE')
+u = advection_solver_sbp_2d_steady(2, 5, 1, 'omega')
